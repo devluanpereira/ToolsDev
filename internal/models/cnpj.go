42e4896c9827bcb23074ee7f7f3e9c7cb5735134
devluanpereira <luan23107@gmail.com>
2024-09-04 14:24:15 -0300

    Corrigido erro Razao Social, Nome Fantasia - undefined.

--- internal/models/cnpj.go
+++ internal/models/cnpj.go
@@ -1,13 +1,13 @@
 package models
 
 type CnpjData struct {
-	Cnpj         string `json:"cnpj"`
-	RazaoSocial  string `json:"razaosocial"`
-	NomeFantasia string `json:"nomefantasia"`
-	Uf           string `json:"uf"`
-	Municipio    string `json:"municipio"`
-	Bairro       string `json:"bairro"`
-	Logradouro   string `json:"logradouro"`
-	Numero       string `json:"numero"`
-	Cep          string `json:"cep"`
+	Cnpj          string `json:"cnpj"`
+	Razao_Social  string `json:"razao_social"`
+	Nome_Fantasia string `json:"nome_fantasia"`
+	Uf            string `json:"uf"`
+	Municipio     string `json:"municipio"`
+	Bairro        string `json:"bairro"`
+	Logradouro    string `json:"logradouro"`
+	Numero        string `json:"numero"`
+	Cep           string `json:"cep"`
 }
