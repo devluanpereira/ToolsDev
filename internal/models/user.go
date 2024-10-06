dddbcff14b3e119270fa4fb0636568712ccf5da7
devluanpereira <luan23107@gmail.com>
2024-10-06 12:07:57 -0300

    Update latest

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,9 +3,10 @@ package models
 import "time"
 
 type User struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-	Password  string    `json:"-"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"-"`
+	//Role      string    `json:"role"`
 	CreatedAt time.Time `json:"created_at"`
 }
